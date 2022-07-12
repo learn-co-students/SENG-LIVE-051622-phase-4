d6d640d75bf1cc982ff989e1740ea4ae59a81ce3
aisayo <aisayo@icloud.com>
2022-07-12 16:05:49 -0400

    Initialize 03_example_starter_client_server_communication_I

---
+++ 03_client_server_communication/flatiron_theater_company/client/src/components/ProductionDetail.js
@@ -0,0 +1,69 @@
+import { useParams } from 'react-router-dom'
+import {useEffect, useState} from 'react'
+import styled from 'styled-components'
+
+function ProductionDetail() {
+  const [production, setProduction] = useState({crew_members:[], performers_and_roles:[]})
+  const params = useParams()
+  
+  useEffect(()=>{
+    //GET to '/productions/:id'
+ 
+  },[])
+  
+ 
+ 
+  const {title, budget, genre, image,description} = production 
+  //Place holder data, will be replaced in the assosiations lecture. 
+  const crew_members = ['Lily-Mai Harding', 'Cathy Luna', 'Tiernan Daugherty', 'Giselle Nava', 'Alister Wallis', 'Aishah Rowland', 'Keiren Bernal', 'Aqsa Parrish', 'Daanyal Laing', 'Hollie Haas']
+  return (
+      <CardDetail>
+        <h1>{title}</h1>
+          <div className='wrapper'>
+            <div>
+              <h3>Genre:</h3>
+              <p>{genre}</p>
+              <h3>Description:</h3>
+              <p>{description}</p>
+              <h2>Crew Memebers</h2>
+              <ul>
+                {crew_members.map(crew => <li>{crew}</li>)}
+              </ul>
+            </div>
+            <img src={image}/>
+          </div>
+      <button> Buy Ticket</button>
+      </CardDetail>
+    )
+  }
+  
+  export default ProductionDetail
+  const CardDetail = styled.li`
+    display:flex;
+    flex-direction:column;
+    justify-content:start;
+    font-family:Arial, sans-serif;
+    margin:5px;
+    h1{
+      font-size:60px;
+      border-bottom:solid;
+      border-color:#42ddf5;
+    }
+    .wrapper{
+      display:flex;
+      div{
+        margin:10px;
+      }
+    }
+    img{
+      width: 300px;
+    }
+    button{
+      background-color:#42ddf5;
+      color: white;
+      height:40px;
+      font-family:Arial;
+      font-size:30px;
+      margin-top:10px;
+    }
+  `
