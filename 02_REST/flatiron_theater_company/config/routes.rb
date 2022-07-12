ad0b8bf91645f87ddb609bf9ba452719a47a162d
aisayo <aisayo@icloud.com>
2022-07-12 16:05:49 -0400

    Initialize 02_example_starter_rest

---
+++ 02_REST/flatiron_theater_company/config/routes.rb
@@ -0,0 +1,26 @@
+Rails.application.routes.draw do
+  resources :productions, only: [:index, :show, :create, :update, :destroy]
+  # Custome Route 
+  # get "/welcome", to: "productions#welcome"
+  # get "/welcome/:id", to: "productions#welcome_show"
+
+  
+  #`rails routes` in the console will show the available routes
+  # only will restrict the routes to specific actions
+  # Bonus 
+  # :as overrides the naming from the route helpers
+  # resources :productions, as: plays
+  
+  # :controller explicitly specifiys a controler
+  # Create a controler called operas to demonstrate
+  # resources :productions, controller: 'operas'
+
+  # namespace allows you to group specific routes 
+  # create a folder in app/controllers/admin 
+  # namespace :admin do
+  #   resources :productions, only: [:create, :update, :delete]
+  # end
+
+  
+
+end
---
+++ 02_REST/flatiron_theater_company/db/seeds.rb
@@ -0,0 +1,11 @@
+p1 = Production.create(title: 'Hamlet', genre: 'Drama', director:'Bill Shakespeare', description:'The Tragedy of Hamlet, Prince of Denmark', budget: 100000.00, image:'https://upload.wikimedia.org/wikipedia/commons/6/6a/Edwin_Booth_Hamlet_1870.jpg', ongoing:true)
+
+p2 = Production.create(title: 'Cats', genre: 'Musical', director:'Andrew Lloyd Webber', description:' Jellicles cats sing and dance', budget: 200000.00, image:'https://upload.wikimedia.org/wikipedia/en/3/3e/CatsMusicalLogo.jpg', ongoing:true)
+
+
+p3 = Production.create(title: 'Carmen', genre: 'Opera', director:'Georges Bizet', description:'Set in southern Spain this is the story of the downfall of Don José, a naïve soldier who is seduced by the wiles of the fiery and beautiful Carmen.', budget: 200000.00, image:'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d4/Prudent-Louis_Leray_-_Poster_for_the_premi%C3%A8re_of_Georges_Bizet%27s_Carmen.jpg/300px-Prudent-Louis_Leray_-_Poster_for_the_premi%C3%A8re_of_Georges_Bizet%27s_Carmen.jpg', ongoing:false)
+
+p3 = Production.create(title: 'Hamilton', genre: 'Musical', director:'Lin-Manuel Miranda', description:'An American Musical is a sung-and-rapped-through musical by Lin-Manuel Miranda. It tells the story of American Founding Father Alexander Hamilton.', budget: 400000.00, image:'https://upload.wikimedia.org/wikipedia/en/thumb/8/83/Hamilton-poster.jpg/220px-Hamilton-poster.jpg', ongoing:false)
+
+
+
