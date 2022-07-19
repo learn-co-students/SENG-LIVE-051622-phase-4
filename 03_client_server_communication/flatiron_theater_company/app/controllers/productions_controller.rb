533a05c2c876ef29a9d370f6f5e2ada3ec2f5eb5
Aysan Isayo <aisayo@icloud.com>
2022-07-19 18:41:29 -0400

    added rescues

--- 03_client_server_communication/flatiron_theater_company/app/controllers/productions_controller.rb
+++ 03_client_server_communication/flatiron_theater_company/app/controllers/productions_controller.rb
@@ -5,9 +5,10 @@ class ProductionsController < ApplicationController
     end 
 
     def show
-         
         production = Production.find(params[:id])
         render json: production, status: :ok
+        rescue ActiveRecord::RecordNotFound => error 
+            render json: {message: error.message}
     end 
 
     def create   
@@ -19,12 +20,16 @@ class ProductionsController < ApplicationController
         production = Production.find(params[:id])
         production.update(production_params)
         render json: production, status: :accepted
+        rescue ActiveRecord::RecordNotFound => error 
+            render json: {message: error.message}
     end 
 
     def destroy
         production = Production.find(params[:id])
         production.destroy
         head :no_content 
+        rescue ActiveRecord::RecordNotFound => error 
+            render json: {message: error.message}
     end 
 
     private
